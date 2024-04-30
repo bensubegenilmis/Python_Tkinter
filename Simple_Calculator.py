0127e3ab7012ee11a62c1007cbcde36ffaaf36cc
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-04-30 09:47:12 +0300

    simplecalculator

---
+++ Simple_Calculator.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Simple Calculator")
+
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+button_1
+
+root.mainloop()
